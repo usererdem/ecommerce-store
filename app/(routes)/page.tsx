4736a9f594318a48e1466fe0afd5a2523a83cabb
erdemelvan <erdemelvan35@gmail.com>
2023-09-04 15:51:55 +0300

    feat: created billboard page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-  return ( <div>hello</div> );
-}
- 
+import Container from "@/components/ui/container";
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/billboard";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("d72c8979-afcb-40f4-8807-9b37fb127082");
+  return (
+    <Container>
+      <div className='space-y-10 pb-10'>
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
+};
+
 export default HomePage;
