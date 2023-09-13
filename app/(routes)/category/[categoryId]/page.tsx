cbf07cad3edcc6b2cedd388528c6619fb61aad6a
erdemelvan <erdemelvan35@gmail.com>
2023-09-13 18:51:19 +0300

    feat: added color filter, added ProductCard component to showcase filtered items, added NoResults component in case of no results

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import getSizes from "@/actions/get-sizes";
 
 import Billboard from "@/components/billboard";
 import Container from "@/components/ui/container";
+import NoResults from "@/components/ui/no-results";
+import ProductCard from "@/components/ui/product-card";
 
 import Filter from "./components/filter";
 
@@ -42,6 +44,15 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
             {/* Add Mobile Filters */}
             <div className='hidden lg:block'>
               <Filter valueKey='sizeId' name='Sizes' data={sizes} />
+              <Filter valueKey='colorId' name='Colors' data={colors} />
+            </div>
+            <div className='mt-6 lg:col-span-4 lg:mt-0'>
+              {products.length === 0 && <NoResults />}
+              <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
