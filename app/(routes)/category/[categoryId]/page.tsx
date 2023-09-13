9081b4dec761fa2d2e1a5f5f96984f099ee6afae
erdemelvan <erdemelvan35@gmail.com>
2023-09-13 19:14:41 +0300

    feat: mobile filters functionality and styling added with mobile-filters component

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -9,6 +9,7 @@ import NoResults from "@/components/ui/no-results";
 import ProductCard from "@/components/ui/product-card";
 
 import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0;
 
@@ -41,7 +42,7 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className='px-4 sm:px-6 lg:px-8 pb-24'>
           <div className='lg:grid lg:grid-cols-5 lg:gap-x-8'>
-            {/* Add Mobile Filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className='hidden lg:block'>
               <Filter valueKey='sizeId' name='Sizes' data={sizes} />
               <Filter valueKey='colorId' name='Colors' data={colors} />
