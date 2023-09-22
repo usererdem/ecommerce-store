1b7c23f0feb74caade23a70c296797cf6b144a35
erdemelvan <erdemelvan35@gmail.com>
2023-09-22 13:31:34 +0300

    feat: checkout button added

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -14,9 +14,29 @@ const Summary = () => {
   const items = useCart((state) => state.items);
   const removeAll = useCart((state) => state.removeAll);
 
+  useEffect(() => {
+    if (searcParams.get("success")) {
+      toast.success("Payment completed.");
+      removeAll();
+    }
+    if (searcParams.get("canceled")) {
+      toast.error("Something went wrong.");
+    }
+  }, [searcParams, removeAll]);
+  
   const totalPrice = items.reduce((total, item) => {
     return total + Number(item.price);
   }, 0);
+
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+    window.location = response.data.url;
+  };
 
   return (
     <div className='mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8'>
@@ -27,7 +47,9 @@ const Summary = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button className='w-full mt-6'>Checkout</Button>
+      <Button onClick={onCheckout} className='w-full mt-6'>
+        Checkout
+      </Button>
     </div>
   );
 };
