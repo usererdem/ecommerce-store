5577ce21753d20e2cee438cd23b4e27028e8d6e5
erdemelvan <erdemelvan35@gmail.com>
2024-03-27 17:41:57 +0300

    fix: final commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Link from "next/link";
 
 import Container from "@/components/ui/container";
@@ -5,11 +7,20 @@ import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 import TechStore from "@/components/navbar-tech-store";
+import { useEffect, useState } from "react";
+import { Category } from "@/types";
 
 export const revalidate = 0;
 
-const Navbar = async () => {
-  const categories = await getCategories();
+const Navbar = () => {
+  const fetchCategories = async () => {
+    setCategories(await getCategories())
+  }
+  const [categories, setCategories] = useState<Category[]>();
+  useEffect(() => {
+    fetchCategories()
+  },[])
+  /* const categories = await getCategories(); */
 
   return (
     <div className='border-b'>
@@ -20,7 +31,7 @@ const Navbar = async () => {
             className='flex lg:ml-0 gap-x-2'>
             <p className='font-bold text-xl py-2 px-3 rounded-full hover:text-gray-50 hover:bg-black transition-all'>CLOTHY</p>
           </Link>
-          <MainNav data={categories} />
+          <MainNav data={categories || []} />
           <TechStore />
           <NavbarActions />
         </div>
